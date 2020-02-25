0035fea2b0c94ab40e076711667cebe97afbf6f0
megalocoman <cfjofre@uc.cl>
2020-02-25 09:44:28 -0300

    clase huevo incluida

---
+++ src/tablero/Huevo.java
@@ -0,0 +1,16 @@
+package tablero;
+
+public class Huevo {
+	
+	public int columna;
+	public int fila;
+	
+	public Huevo(int columna, int fila) {
+		
+		this.columna = columna;
+		this.fila = fila;
+	}
+	
+	// generar metodo que tome puntaje y lo ponga como atributo de la clase.
+	
+}
--- src/tablero/Tablero.java
+++ src/tablero/Tablero.java
@@ -1,7 +1,5 @@
 package tablero;
 
-
-	
 	public class Tablero {	
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub		
